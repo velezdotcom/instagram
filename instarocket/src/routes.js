bd0d942cf72d086eb11ab51ee39d081e3acb6b3e
Gabriel <gabrieuvelezmoro@gmail.com>
2020-05-06 20:21:15 -0300

    update

---
+++ instarocket/src/routes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {Image} from 'react-native';
+
+import Feed from './pages/Feed';
+import New from './pages/New';
+
+import logo from './assets/logo.png';
+
+export default createAppContainer(
+  createStackNavigator(
+    {
+      Feed,
+      New,
+    },
+    {
+      defaultNavigationOptions: {
+        headerTintColor: '#000',
+        headerTitle: <Image style={{marginHorizontal: 20}} source={logo} />,
+        headerBackTitle: null,
+      },
+      mode: 'modal',
+    },
+  ),
+);
